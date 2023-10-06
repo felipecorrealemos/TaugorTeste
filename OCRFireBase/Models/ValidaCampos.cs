e4df04656da94cb5cfc88eaf4e10239e1b1501f8
Felipe Correa <58709080+felipecorrealemos@users.noreply.github.com>
2023-10-06 01:48:32 -0300

    .

---
+++ OCRFireBase/Models/ValidaCampos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OCRFireBase.Models
+{
+    public class ValidaCampos
+    {
+        public static string ValidaCamposDigitados(string digitacao)
+        {
+            digitacao = Regex.Replace(digitacao, @"[^\w\d]", "");
+            return digitacao;
+        }
+    }
+}
