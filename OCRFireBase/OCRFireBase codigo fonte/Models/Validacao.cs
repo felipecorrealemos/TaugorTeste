73f011d69789a68b4c49da737183a8200b0a13d1
Felipe Correa <58709080+felipecorrealemos@users.noreply.github.com>
2023-10-11 07:24:54 -0300

    .

---
+++ OCRFireBase/OCRFireBase codigo fonte/Models/Validacao.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OCRFireBase.Models
+{
+    public class Validacao
+    {
+        public static string ValidaCamposDigitados(string digitacao)
+        {
+            digitacao = Regex.Replace(digitacao, @"[^\w\d]", "");
+            return digitacao;
+        }
+
+        public static bool ValidaPasta(string pastaTemp)
+        {
+            string appDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "OCRFireBase");
+            appDataPath = Path.Combine(appDataPath,pastaTemp);
+
+            if (!Directory.Exists(appDataPath))
+            {
+                
+                Directory.CreateDirectory(appDataPath);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool VerificaSeArquivoDisponivel(string caminho)
+        {
+            try
+            {
+                using (FileStream stream = File.Open(caminho, FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    stream.Close();
+                }
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ OCRFireBase/OCRFireBase codigo fonte/Views/SplashScreen.cs
@@ -0,0 +1,87 @@
+﻿using Google.Cloud.Firestore;
+using OCRFireBase.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OCRFireBase.Views
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Validacao.ValidaPasta("temp");
+                Validacao.ValidaPasta("thumbnail");
+            }
+
+            catch (Exception ex) { MessageBox.Show(ex.Message, "Nao foi possivel criar as pasta temp"); }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void Login()
+        {
+            // Initialize Firebase
+            try
+            {
+                Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", CredenciaisFireBase.arquivoCredencialJson);
+                CollectionReference colRef = CredenciaisFireBase.GetCollectionRef();
+                DocumentSnapshot doc = colRef.Document(CredenciaisFireBase.colecao).GetSnapshotAsync().Result;
+
+                if (doc.Exists)
+                {
+                    Dictionary<string, object> data = doc.ToDictionary();
+
+                    if (data.TryGetValue("usuario", out object usuarioValue) && data.TryGetValue("senha", out object senhaValue))
+                    {
+                        string usuario = usuarioValue.ToString();
+                        string senha = senhaValue.ToString();
+
+                        string usuarioDigitado = Validacao.ValidaCamposDigitados(textBox1.Text.Trim());
+                        string senhaDigitada = Validacao.ValidaCamposDigitados(textBox2.Text.Trim());
+
+                        if (usuarioDigitado == usuario && senhaDigitada == senha)
+                        {
+                            this.Hide();
+
+                            FormPrincipal principal = new FormPrincipal();
+                            principal.Show();
+                            return;
+                        }
+
+                        label1.Visible = true;
+                    }
+                }
+            }
+
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+    }
+}
